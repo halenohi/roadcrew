2392779721851665e2924d0137afb2aa6ebc3649
achm <achm.pppp@gmail.com>
2014-10-15 16:53:54 +0900

    Roadcrew::Configを実装

---
+++ lib/roadcrew/config.rb
@@ -0,0 +1,37 @@
+module Roadcrew
+  def self.configure(&block)
+    @config = Config.new(&block)
+  end
+
+  def self.configuration
+    @config
+  end
+
+  class Config
+    attr_reader :garages
+
+    def initialize(&block)
+      @garages = Garages.new
+      instance_eval(&block)
+    end
+
+    def garage(hash)
+      @garages.add hash
+    end
+
+    class Garages
+      def initialize
+        @collection = {}
+      end
+
+      def add(hash)
+        options = hash.to_a[0]
+        @collection.store(options[0], options[1])
+      end
+
+      def [](name)
+        @collection[name]
+      end
+    end
+  end
+end
---
+++ spec/roadcrew/config_spec.rb
@@ -0,0 +1,12 @@
+require 'roadcrew_spec'
+
+describe Roadcrew::Config do
+  let(:config) { Roadcrew::Config.new {} }
+
+  describe '#garage' do
+    it 'ハッシュを@garagesに追加すること' do
+      config.garage({ sample_name: { endpoint: 'http://example.com' } })
+      expect(config.garages[:sample_name]).to eq({ endpoint: 'http://example.com' })
+    end
+  end
+end
