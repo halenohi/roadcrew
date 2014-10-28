ebcb959d051904bd8cf2d2122785ebfe5a9487ca
achm <achm.pppp@gmail.com>
2014-10-28 14:50:53 +0900

    Railsのfragment cacheを利用するようにした

--- lib/roadcrew/connection.rb
+++ lib/roadcrew/connection.rb
@@ -2,12 +2,15 @@ module Roadcrew
   class Connection
     REST_ACTIONS = %w(get post put patch delete)
 
+    attr_accessor :cache_expires_in
+
     def initialize(options)
       @client_id = options[:client_id]
       @client_secret = options[:client_secret]
       @site = options[:endpoint]
       @token = options[:access_token]
       @oauth_client = OAuth2::Client.new(@client_id, @client_secret, site: @site)
+      @cache_expires_in = options[:cache_expires_in] || 1.hours
     end
 
     def access_token
@@ -16,7 +19,13 @@ module Roadcrew
 
     def method_missing(method, *args)
       if REST_ACTIONS.include? method.to_s
-        access_token.send(method, *args)
+        if Rails.cache
+          Rails.cache.fetch "Roadcrew_#{ @site }#{ args[0] }_#{ method }", expires_in: cache_expires_in, skip_digest: true do
+            access_token.send(method, *args)
+          end
+        else
+          access_token.send(method, *args)
+        end
       else
         super
       end
