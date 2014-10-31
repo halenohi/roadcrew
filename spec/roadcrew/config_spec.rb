228e95f24ac2f3e49cc522fb19909d46a82569ea
achm <achm.pppp@gmail.com>
2014-10-31 17:39:40 +0900

    configureで既定値があった場合にマージするように変更

--- spec/roadcrew/config_spec.rb
+++ spec/roadcrew/config_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe Roadcrew::Config do
-  let(:config) { Roadcrew::Config.new {} }
+  let(:config) { Roadcrew::Config.new }
 
   describe '#garage' do
     it 'ハッシュを@garagesに追加すること' do
       config.garage({ sample_name: { endpoint: 'http://example.com' } })
       expect(config.garages[:sample_name]).to eq({ endpoint: 'http://example.com' })
+      config.garage({ sample_name: { endpoint: 'http://example-2.com' } })
+      expect(config.garages[:sample_name]).to eq({ endpoint: 'http://example-2.com' })
     end
   end
 end
