e02ae12788dd6df63a80d3a0e9baacaa1972a5f8
kozo yamagata <tune002@gmail.com>
2014-11-20 18:13:57 +0900

    roadcrew.rbでcontroller_helpersとengineをrequireする

--- lib/roadcrew.rb
+++ lib/roadcrew.rb
@@ -5,6 +5,8 @@ require "oauth2/access_token"
 require "roadcrew/config"
 require "roadcrew/client"
 require "roadcrew/connection"
+require "roadcrew/controller_helpers"
+require "roadcrew/engine"
 require_relative "../config/endpoints"
 
 module Roadcrew
