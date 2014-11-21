50b11e574bd950b054f13a04fb2bd4853ef70e77
kozo yamagata <tune002@gmail.com>
2014-11-21 11:28:00 +0900

    Authenticatorを追加する

---
+++ lib/roadcrew/authenticator.rb
@@ -0,0 +1,41 @@
+module Roadcrew
+  class Authenticator
+    class << self
+      def authenticate!(auth = {})
+        auth = {} if auth.nil?
+        admin = admin_class.new(auth['token'])
+        raise_error if auth['token'] == nil || !admin.logged_in?
+      end
+
+      def login!(credentials)
+        admin = admin_class.new
+        token = admin.login(credentials)
+
+        auth = token ? { 'token' => token } : false
+        yield(auth)
+
+        raise_error unless auth
+        auth
+      end
+
+      def logout(auth)
+        admin_class.new(auth[:token]).logout
+      end
+
+      def raise_error
+        raise Roadcrew::NotAuthenticatedError.new
+      end
+
+      def admin_class
+        unless @admin_class_garagelized
+          Models::Admin.tap do |a|
+            a.garage :admin
+            a.base_path '/api'
+          end
+          @admin_class_garagelized = true
+        end
+        Models::Admin
+      end
+    end
+  end
+end
