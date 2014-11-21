961a4de000ab714943b01d7e9fbd943e93db286a
kozo yamagata <tune002@gmail.com>
2014-11-21 13:31:28 +0900

    spec/supportフォルダ内のrbファイルを読み込むようにする

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rails'
 require 'active_model'
 require 'roadcrew'
+
+Dir[File.expand_path("../support/**/*.rb", __FILE__)].each { |f| require f }
