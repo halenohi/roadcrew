ec7d4bdcf63cfaa89d841d4c3811dc229d51fd6a
kozo yamagata <tune002@gmail.com>
2014-11-21 11:27:42 +0900

    Authenticator、Models::Admin、NotAuthenticatedErrorを追加する

--- lib/roadcrew.rb
+++ lib/roadcrew.rb
@@ -2,12 +2,15 @@ require "rails"
 require "oauth2/client"
 require "oauth2/access_token"
 
+require "roadcrew/authenticator"
 require "roadcrew/config"
 require "roadcrew/client"
 require "roadcrew/connection"
 require "roadcrew/controller_helpers"
 require "roadcrew/engine"
+require "roadcrew/models/admin"
 require_relative "../config/endpoints"
 
 module Roadcrew
+  class NotAuthenticatedError < StandardError; end
 end
