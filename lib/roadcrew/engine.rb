5d5552e2ce12f0593c291d460d48d258ee7840c3
kozo yamagata <tune002@gmail.com>
2014-11-21 18:46:02 +0900

    engineに isolate_namespace Roadcrew を追加する

--- lib/roadcrew/engine.rb
+++ lib/roadcrew/engine.rb
@@ -1,5 +1,7 @@
 module Roadcrew
   class Engine < ::Rails::Engine
+    isolate_namespace Roadcrew
+
     initializer 'roadcrew.action_controller' do |app|
       ActiveSupport.on_load(:action_controller) do
         include Roadcrew::ControllerHelpers
