8773270229a01d6e2cd3881d4f5d4012032c1f74
kozo yamagata <tune002@gmail.com>
2015-05-07 17:46:58 +0900

    Add sample configuration code

--- config/endpoints.rb
+++ config/endpoints.rb
@@ -1,15 +1,21 @@
 Roadcrew.configure do
   garage admin: {
     endpoint: 'https://staart-admin.herokuapp.com',
-    access_token: ENV['STAART_ADMIN_ACCESS_TOKEN']
+    access_token: ENV['STAART_ADMIN_ACCESS_TOKEN'],
+    expires_at: ENV['STAART_ADMIN_EXPIRES_AT'],
+    refresh_token: ENV['STAART_ADMIN_REFRESH_TOKEN']
   }
   garage analytics: {
     endpoint: 'https://staart-analytics.herokuapp.com',
-    access_token: ENV['STAART_ANALYTICS_ACCESS_TOKEN']
+    access_token: ENV['STAART_ANALYTICS_ACCESS_TOKEN'],
+    expires_at: ENV['STAART_ADMIN_EXPIRES_AT'],
+    refresh_token: ENV['STAART_ADMIN_REFRESH_TOKEN']
   }
   garage scheduler: {
     endpoint: 'https://staart-scheduler.herokuapp.com',
-    access_token: ENV['STAART_SCHEDULER_ACCESS_TOKEN']
+    access_token: ENV['STAART_SCHEDULER_ACCESS_TOKEN'],
+    expires_at: ENV['STAART_ADMIN_EXPIRES_AT'],
+    refresh_token: ENV['STAART_ADMIN_REFRESH_TOKEN']
   }
   garage twitter: {
     endpoint: 'https://staart-twitter.herokuapp.com',
